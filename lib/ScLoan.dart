16bc970c915d2b5849afa0160ae2ad1fc08493c3
Indrajit Roy <70997867+indrajit-roy@users.noreply.github.com>
2023-10-18 05:36:16 +0530

    feat: servicing intent dart

--- lib/ScLoan.dart
+++ lib/ScLoan.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/services.dart';
 
 enum SCLoanEnvironment { PRODUCTION, DEVELOPMENT, STAGING }
+
 class SCLoans {
   static const MethodChannel _channel =
-  const MethodChannel('scloans_flutter_plugin');
+      const MethodChannel('scloans_flutter_plugin');
 
   static Future<String?> setup(
-      SCLoanEnvironment environment,
-      String gateway,
-      ) async {
+    SCLoanEnvironment environment,
+    String gateway,
+  ) async {
     String? setupResponse;
 
     try {
-      setupResponse = await _channel.invokeMethod('setup', <String, dynamic>{
-        "gateway": gateway,
-        "env": environment.index
-      });
+      setupResponse = await _channel.invokeMethod('setup',
+          <String, dynamic>{"gateway": gateway, "env": environment.index});
     } on PlatformException catch (e) {
       setupResponse = e.code;
     }
@@ -24,8 +23,8 @@ class SCLoans {
   }
 
   static Future<String?> apply(
-      String interactionToken,
-      ) async {
+    String interactionToken,
+  ) async {
     String? applyResponse;
 
     try {
@@ -40,12 +39,12 @@ class SCLoans {
   }
 
   static Future<String?> pay(
-      String interactionToken,
-      ) async {
+    String interactionToken,
+  ) async {
     String? payResponse;
 
     try {
-      payResponse = await _channel.invokeMethod('apply', <String, dynamic>{
+      payResponse = await _channel.invokeMethod('pay', <String, dynamic>{
         "interactionToken": interactionToken,
       });
     } on PlatformException catch (e) {
@@ -56,12 +55,13 @@ class SCLoans {
   }
 
   static Future<String?> withdraw(
-      String interactionToken,
-      ) async {
+    String interactionToken,
+  ) async {
     String? withdrawResponse;
 
     try {
-      withdrawResponse = await _channel.invokeMethod('apply', <String, dynamic>{
+      withdrawResponse =
+          await _channel.invokeMethod('withdraw', <String, dynamic>{
         "interactionToken": interactionToken,
       });
     } on PlatformException catch (e) {
@@ -69,5 +69,22 @@ class SCLoans {
     }
 
     return withdrawResponse;
+  }
+
+  static Future<String?> service(
+    String interactionToken,
+  ) async {
+    String? serviceResponse;
+
+    try {
+      serviceResponse =
+          await _channel.invokeMethod('service', <String, dynamic>{
+        "interactionToken": interactionToken,
+      });
+    } on PlatformException catch (e) {
+      serviceResponse = e.code;
+    }
+
+    return serviceResponse;
   }
 }
