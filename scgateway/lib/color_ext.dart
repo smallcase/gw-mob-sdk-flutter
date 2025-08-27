0ec9c2e28095b102e7dc2961243f5a3a14f5b5c3
indrajit-roy-sc <indrajit.roy@smallcase.com>
2025-08-27 14:00:19 +0530

    chore(scg): address `pub publish` warnings

--- scgateway/lib/color_ext.dart
+++ scgateway/lib/color_ext.dart
@@ -1,10 +1,12 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/widgets.dart';
 
 extension ColorExt on Color {
   String toHex({bool leadingHashSign = true, bool includeAlpha = false}) =>
       '${leadingHashSign ? '#' : ''}'
-          '${includeAlpha ? alpha.toRadixString(16).padLeft(2, '0') : ''}'
-          '${red.toRadixString(16).padLeft(2, '0')}'
-          '${green.toRadixString(16).padLeft(2, '0')}'
-          '${blue.toRadixString(16).padLeft(2, '0')}';
+      '${includeAlpha ? alpha.toRadixString(16).padLeft(2, '0') : ''}'
+      '${red.toRadixString(16).padLeft(2, '0')}'
+      '${green.toRadixString(16).padLeft(2, '0')}'
+      '${blue.toRadixString(16).padLeft(2, '0')}';
 }
